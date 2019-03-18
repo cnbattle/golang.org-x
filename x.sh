1813ff48dedc6c145158045f24e4dd279bef170a
李启爱 <liqiai@live.com>
2019-03-18 09:49:03 +0800

    增加GOPATH环境变量检测

--- x.sh
+++ x.sh
@@ -1,5 +1,16 @@
 #!/bin/bash 
 
+red='\e[91m'
+green='\e[92m'
+yellow='\e[93m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+
+if [$GOPATH == ""];then
+    echo -e "${red}不存在GOPATH环境变量, 请设置后再次执行次脚本${none}" && exit 1
+fi
+
 packages=(
     "blog"
     "crypto"
